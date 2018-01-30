ac0e5ab762e37bb51c9ac9e39b961ec6e8f1d6f0
Torgeir Baraldsnes <torgeiba@ifi.uio.no>
2018-01-31 00:36:15 +0100

    Implemented makemlp_asm procedure. Testing and potential bugfixing remains.

--- AssemblyMultilayerPerceptron/AssemblyMultilayerPerceptron/asmmlp.asm
+++ AssemblyMultilayerPerceptron/AssemblyMultilayerPerceptron/asmmlp.asm
@@ -29,6 +29,12 @@ extern vecfree_asm:proc
 extern matfree_asm:proc
 extern submat_asm:proc
 extern scalemat_asm:proc
+extern vecrand_asm:proc
+extern veczeros_asm:proc
+extern matzeros_asm:proc
+extern matrand_asm:proc
+extern matones_asm:proc
+
 
 
 
@@ -80,55 +86,301 @@ extern scalemat_asm:proc
 ; x64 architecture:   https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/x64-architecture
 ; Register usage:     https://msdn.microsoft.com/en-us/library/9z1stfyw.aspx
 
+
+; vec* get_vecarray_element(vec* array, uint64 index);
+get_vecarray_element proc
+	shl rdx, 4
+	add rcx, rdx
+	mov rax, rcx
+	ret
+get_vecarray_element endp
+
+
 ; extern mlp* makemlp_asm(uint64 numlayers, uint64* layersizes, uint64 numepochs, float learningrate);
 ; input rcx, rdx, r8, r9
 ; output rax
-makemlp_asm proc
-	; mlp* result = malloc(sizeof(mlp));
-	; 
+makemlp_asm proc	; struct mlp {
+					;  	uint64 numlayers;	+ 0
+					;  	vec* layers;		+ 8		
+					;  	vec* biases;		+ 16		
+					;  	mat* weights;		+ 24		
+					;  	vec* layersErr;		+ 32		
+					;		
+					;  	vec* biasesErr;		+ 40		
+					;  	mat* weightsErr;	+ 48		
+					;				
+					;  	float learningrate; + 56 ; note! beware of possible padding! TODO: Check this				
+					;  	uint64 numepochs;   + 64 ; prev was 4 byte float (?)				
+	push r12; l / w					
+	push r13; numlayers / numweights				
+	push r14; nonvolatile scratch register
+	push r15; mlp* net / result							
+	push rbx; &layersizes[0]
+
+	mov rbx, rdx ; rbx = layersizes&[0]
+
+	; push arguments until after mlp malloc call
+	push rcx
+	push rdx
+	push r8
+	sub rsp, 4;
+	movd rcx, xmm3
+	mov [rsp], rcx
+
+	; mlp* result = malloc(sizeof(mlp));												
+	mov rcx, 72; sizeof(mlp) = 72						
+	call malloc_asm ;								
+	mov r15, rax ; r15 = mlp* result / net; 			
+
+	mov rcx, [rsp]
+	movd xmm3, rcx
+	add rsp, 4;
+	pop rdx
+	pop r8
+	pop rcx
+
+	mov r13, rcx; r13 = numlayers 	; uint64 numweights = numlayers - 1;
+
 	; result->numlayers = numlayers;
-	; result->learningrate = learningrate;
+	mov [r15 + 0], r13;
+
+	; result->learningrate = learningrate; learningrate; + 56
+	movd rcx, xmm3
+	mov [r15 + 56], rcx
+
 	; result->numepochs = numepochs;
-	; 
-	; uint64 numweights = numlayers - 1;
-	; 
+	mov [r15 + 64], r8
+
+
 	; result->layers = (vec*)malloc(numlayers * sizeof(vec));
-	; result->layersErr = (vec*)malloc(numlayers * sizeof(vec));
-	; 
-	; result->weights = (mat*)malloc(numweights * sizeof(mat));
-	; result->weightsErr = (mat*)malloc(numweights * sizeof(mat));
-	; 
-	; result->biases = (vec*)malloc(numweights * sizeof(vec));
+	mov rcx, r13; numlayers
+	shl rcx, 4  ; * sizeof(vec)
+	call malloc_asm ;
+	mov [r15 + 8], rax;	+offset(mlp, layers)							
+																		
+	; result->layersErr = (vec*)malloc(numlayers * sizeof(vec));		
+	mov rcx, r13; numlayers
+	shl rcx, 4  ; * sizeof(vec)													
+	call malloc_asm ;
+	mov [r15 + 32], rax; +offset(mlp, layersErr)
+																		
+	; result->weights = (mat*)malloc(numweights * sizeof(mat));			
+	mov rcx, r13; numlayers
+	dec rcx		; numweights = numlayers - 1
+	shl rcx, 1  ; * 2
+	add rcx, r13; + numlayers
+	dec rcx		; numweights = numlayers - 1
+	shl rcx, 3  ; * 8 = (numweights*3) * 8 = numweights * sizeof(mat)													
+	call malloc_asm ;
+	mov [r15 + 24], rax;	+offset(mlp, weights)												
+																		
+	; result->weightsErr = (mat*)malloc(numweights * sizeof(mat));		
+	mov rcx, r13; numlayers
+	dec rcx		; numweights = numlayers - 1
+	shl rcx, 1  ; * 2
+	add rcx, r13; + numlayers
+	dec rcx		; numweights = numlayers - 1
+	shl rcx, 3  ; * 8 = (numweights*3) * 8 = numweights * sizeof(mat)													
+	call malloc_asm ;
+	mov [r15 + 48], rax;	+offset(mlp, weightsErr)												
+												
+	; result->biases = (vec*)malloc(numweights * sizeof(vec));			
+	mov rcx, r13; numlayers
+	dec rcx		; numweights = numlayers - 1
+	shl rcx, 4  ; * sizeof(vec)
+	call malloc_asm ;
+	mov [r15 + 16], rax;	+offset(mlp, biases)
+
 	; result->biasesErr = (vec*)malloc(numweights * sizeof(vec));
-	; 
+	mov rcx, r13; numlayers
+	dec rcx		; numweights = numlayers - 1
+	shl rcx, 4  ; * sizeof(vec)
+	call malloc_asm ;
+	mov [r15 + 40], rax;	+offset(mlp, biasesErr)
+
 	; uint64 l = 0;
-	; while (l < numlayers) {
-	; 	result->layers[l] = veczeros_asm(layersizes[l]);
-	; 	result->layersErr[l] = veczeros_asm(layersizes[l]);
-	; 	l++;
+	xor r12, r12 ; r12 = l = 0
+	mlp_layer_initialize: ; while (l < numlayers) {
+		
+		mov rcx, r12; Hidden return param &result->layers[l]
+		shl rcx, 4  ; sizeof(vec) = 16 byte = 2^4 = 1 << 4
+		add rcx, [r15 + 8] ; += &result->layers[0] (&mlp+8)
+
+		mov rdx, r12; layersizes[l]
+		shl rdx, 3  ; sizeof(unint64) = 8 byte = 2^3 = 1 << 3
+		add rdx, rbx; += &layersizes[0]
+		call veczeros_asm ; result->layers[l] = veczeros_asm(layersizes[l]);
+		
+		mov rcx, r12; Hidden return param &result->layersErr[l]
+		shl rcx, 4  ; sizeof(vec) = 16 byte = 2^4 = 1 << 4
+		add rcx, [r15 + 32] ; += &result->layersErr[0] (&mlp+32)
+
+		mov rdx, r12; layersizes[l]
+		shl rdx, 3  ; sizeof(unint64) = 8 byte = 2^3 = 1 << 3
+		add rdx, rbx; += &layersizes[0]
+		call veczeros_asm ; result->layersErr[l] = veczeros_asm(layersizes[l]);
+	
+	inc r12; 	l++
+	cmp r12, r13; (l < numlayers)
+	jl mlp_layer_initialize
 	; }
-	; 
+
 	; uint64 w = 0;
-	; while (w < numweights) {
-	; 	mat m = matrand_asm(layersizes[w + 1], layersizes[w]);
-	; 	mat m1 = matones_asm(m.rows, m.cols);
-	; 	scalemat_asm(.5f, &m1, &m1);
-	; 	submat_asm(&m, &m1, &m);
-	; 	result->weights[w] = m;
-	; 	result->weightsErr[w] = matzeros_asm(layersizes[w + 1], layersizes[w]);
-	; 	matfree_asm(&m1);
-	; 
-	; 	vec v = vecrand_asm(layersizes[w + 1]);
-	; 	vec v1 = vecones_asm(v.size);
-	; 	scalevec_asm(.5f, &v1, &v1);
-	; 	subvec_asm(&v, &v1, &v);
-	; 	result->biases[w] = v;
-	; 	result->biasesErr[w] = veczeros_asm(layersizes[w + 1]);
-	; 	vecfree_asm(&v1);
-	; 
-	; 	w++;
+	xor r12, r12 ; r12 = w = 0
+	dec r13 ; r13 = numweights = layersizes - 1
+	mlp_weights_initialize: ; while (w < numweights) {
+		
+		sub rsp, 24 ; alloc m
+
+		mov r14, rsp ; r14 temp = &m
+
+		mov rcx, r14 ; hidden return param = &m
+		mov rdx, r12; layersizes[w+1]
+		inc rdx;
+		shl rdx, 3; sizeof(uint64) = 8 bit = 2^3 = 1 << 3
+		add rdx, rbx; = (w+1) * sizeof(uint64) + &layersizes[0]
+		
+		mov r8,  r12; layersizes[w]
+		shl r8, 3; sizeof(uint64) = 8 byte = 2^3 = 1 << 3
+		add r8, rbx; = w * sizeof(uint64) + &layersizes[0]
+		; 	mat m = matrand_asm(layersizes[w + 1], layersizes[w]);
+		call matrand_asm ;
+
+
+		; 	mat m1 = matones_asm(m.rows, m.cols);
+		sub rsp, 24 ; alloc m1
+		mov rcx, rsp ; Hidden return variable &m1
+		mov rdx, [r14 + 8]; m.rows
+		mov r8, [r14 + 16]; m.cols
+		call matones_asm ; 
+
+		mov rcx, 3Fh
+		movd xmm0, rcx; 0.5f (IEEE single precision) literal
+		mov rdx, rsp; &m1
+		mov rcx, rdx; &m1
+		call scalemat_asm ; 	scalemat_asm(.5f, &m1, &m1);
+
+		mov rcx, r14; &m
+		mov rdx, rsp; &m1
+		mov r8 , rcx; &m
+		call submat_asm ; 	submat_asm(&m, &m1, &m);
+
+
+		; 	result->weights[w] = m;
+		; r14 = &m
+		; mov data, rows, cols
+		mov rdx, r12
+		shl rdx, 1
+		add rdx, r12
+		shl rdx, 3 ; rdx = sizeof(mat) * w
+
+		add rdx, r15; += &mlp
+		add rdx, 24 ; += offsetof(result,weights); rdx = &result->weights[w]
+
+		mov rcx, [r14 + 0]; m.data (+0)
+		mov [rdx + 0], rcx; result->weights[w].data(+0)
+		mov rcx, [r14 + 8]; m.rows (+8)
+		mov [rdx + 8], rcx ; result->weights[w].rows (+8)
+		mov rcx, [r14 + 16]; m.cols (+16)
+		mov [rdx + 16], rcx ; result->weights[w].cols (+16)
+
+		; 	result->weightsErr[w] = matzeros_asm(layersizes[w + 1], layersizes[w]);
+		mov rcx, r12
+		shl rcx, 1
+		add rcx, r12
+		shl rcx, 3
+		add rcx, r15; += &mlp
+		add rcx, 48; += offsetof(mlp, weightsErr) ;(+48); hidden return parameter &result->weightsErr[w]
+
+		mov rdx, r12; layersizes[w+1]
+		inc rdx;
+		shl rdx, 3; sizeof(uint64) = 8 byte = 2^3 = 1 << 3
+		add rdx, rbx; = (w+1) * sizeof(uint64) + &layersizes[0]
+		
+		mov r8,  r12; layersizes[w]
+		shl r8, 3; sizeof(uint64) = 8 byte = 2^3 = 1 << 3
+		add r8, rbx; = w * sizeof(uint64) + &layersizes[0]
+		call matzeros_asm ;
+
+		mov rcx, rsp; &m1
+		call matfree_asm ; 	matfree_asm(&m1);
+
+		add rsp, 24 ; dealloc m1
+		add rsp, 24 ; dealloc m
+
+		; 	vec v = vecrand_asm(layersizes[w + 1]);
+		sub rsp, 16 ; alloc v
+		mov r14, rsp; r14 temp = &v
+		sub rsp, 16 ; alloc v1
+
+		mov rcx, r14; hidden return parameter &v
+		mov rdx, r12; layersizes[w+1]
+		inc rdx;
+		shl rdx, 3; sizeof(uint64) = 8 byte = 2^3 = 1 << 3
+		add rdx, rbx; = (w+1) * sizeof(uint64) + &layersizes[0]
+		call vecrand_asm
+
+		; 	vec v1 = vecones_asm(v.size);
+		mov rdx, [r14 + 8]; v.size
+		mov rcx, rsp ; hidden return parameter &v1
+		call vecones_asm
+
+		mov rcx, 3Fh
+		movd xmm0, rcx; 0.5f (IEEE single precision) literal
+		mov rdx, rsp; &v1
+		mov rcx, rdx; &v1
+		call scalevec_asm ; 	scalevec_asm(.5f, &v1, &v1);
+
+		mov rcx, r14; &v
+		mov rdx, rsp; &v1
+		mov r8 , rcx; &v
+		call subvec_asm ; 	subvec_asm(&v, &v1, &v);
+
+		; 	result->biases[w] = v;
+		; r14 = &v
+		; mov data, size
+		mov rdx, r12
+		shl rdx, 4 ; rdx = sizeof(vec) * w
+		add rdx, r15; rdx += &mlp
+		add rdx, 16 ; += offsetof(result,biases); rdx = &result->biases[w]
+
+		mov rcx, [r14 + 0]; m.data (+0)
+		mov [rdx + 0], rcx; result->biases[w].data(+0)
+		mov rcx, [r14 + 8]; m.size (+8)
+		mov [rdx + 8], rcx ; result->biases[w].size (+8)
+
+
+		; 	result->biasesErr[w] = veczeros_asm(layersizes[w + 1]);
+		mov rcx, r12 ; w
+		inc rcx      ; w + 1
+		shl rcx, 4   ; (w+1) * sizeof(vec) ; (16 byte  = 1 << 4)
+		add rcx, r15; += &mlp
+		add rcx, 40; += offsetof(mlp, biaserr) (+40); rcx = hidden return parameter &(result->biasesErr[w])
+
+		mov rdx, r12; layersizes[w+1]
+		inc rdx;
+		shl rdx, 3; sizeof(uint64) = 8 byte = 2^3 = 1 << 3
+		add rdx, rbx; = (w+1) * sizeof(uint64) + &layersizes[0]
+		call veczeros_asm
+
+		mov rcx, rsp; &v1
+		call vecfree_asm ; 	vecfree_asm(&v1);
+ 
+		add rsp, 32 ; dealloc v, v1
+		
+	inc r12; w++;
+	cmp r12, r13; (w < numweights)
+	jl mlp_weights_initialize
 	; }
-	; return result;
+	
+	mov rax, r15 ; return result;
+
+	pop rbx
+	pop r15;
+	pop r14;
+	pop r13;
+	pop r12;
+
 	ret
 makemlp_asm endp
 
@@ -510,7 +762,7 @@ fwd_asm proc
 	ret
 fwd_asm endp
 
-; TODO: Seems to have a bug giving wrong output.
+; TODO: Seems to have a bug giving wrong output. // update: seems to work now!
 ; Might be a problem with the number of iterations or learning rate
 ; extern void bwd_asm(mlp* net, vec* output);
 ; input:
